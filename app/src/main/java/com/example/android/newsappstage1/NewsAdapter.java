e3b91b20efda8130db06ddb336ebf4606e79814a
lehieu30794 <lehieu30794@gmail.com>
2018-08-15 11:29:59 -0600

    Working NewsAdapter

---
+++ app/src/main/java/com/example/android/newsappstage1/NewsAdapter.java
@@ -0,0 +1,32 @@
+package com.example.android.newsappstage1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class NewsAdapter extends ArrayAdapter<News> {
+    public NewsAdapter(Activity context, List<News> news) {
+        super(context, 0, news);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_news_item,parent, false);
+        }
+
+
+        return convertView;
+    }
+}
